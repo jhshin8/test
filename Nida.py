d7e9cdf5f56e98c1ea08efb96815fffaaf8073c0
jhshin8 <140725127+jhshin8@users.noreply.github.com>
2025-03-06 14:57:58 -0500

    Update Nida.py

--- Nida.py
+++ Nida.py
@@ -10,27 +10,7 @@ chrome_options.add_argument ("--start-maximized")
 driver = webdriver.Chrome (options = chrome_options)
 set_driver (driver)                     # this tells helium to use the selenium webdriver instance
 
-go_to (r"https://bdc-demo.fcc.gov/")
+go_to (r"https://www.google.com")
 
-wait_until(Button("Agree").exists, timeout_secs=20)
 
-click("Agree")
-click("Sign in")
-write('u.lsmperfte.st@gmail.com', into="Username")
-wait_until(Button("Next").exists)
-click("next")
-write("kAc&a-hux5puq9", into="Password")
-click("Sign in")
-click("0029429628")
-click("Create Submission")
-
-#click(Link("Download"))
-# element = driver.find_element(By.CSS_SELECTOR, ".border-0")
-# element.click()
-#click(Text("Download zipped Provider Summary file"))
-
-#try this code than driver find element
-#don't use Selenium codes. Use helium codes
-#study it
-# sleep(5)
 quit()
